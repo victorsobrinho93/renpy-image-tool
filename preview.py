38ebe0c5814eda5e1f60a9179cee6a0e405f0504
Victor Sobrinho <victorhso@outlook.com>
2022-07-31 09:44:03 -0300

    Fixed issue with scene preview.

--- preview.py
+++ preview.py
@@ -11,24 +11,22 @@ class Preview(Toplevel):
         self.iconbitmap("src/icon.ico")
         self.frames = cycle(ImageTk.PhotoImage(Image.open(frame)) for frame in self.controller.frames)
         self.timing = int(float(timing.get()) * 1000)
-        # self.output = timing
+        self.entry = timing
 
     def player(self):
-
-        self.slow = Button(self, text="-100 ms", command=lambda: self.decrease_timing(100))
-        self.slow.grid(row=2, column=7, sticky=E)
-        self.v_slow = Button(self, text="-10 ms", command=lambda: self.decrease_timing(10))
-        self.v_slow.grid(row=2, column=8, sticky=E)
-        self.apply_timing = Button(self, text="Apply New Timing", command=self.apply_timing)
-        self.apply_timing.grid(row=2, column=9, columnspan=5, sticky=EW, padx=5)
-        self.v_fast = Button(self, text="+10 ms", command=lambda: self.increase_timing(10))
-        self.v_fast.grid(row=2, column=14, sticky=W)
-        self.fast = Button(self, text="+100 ms", command=lambda: self.increase_timing(100))
-        self.fast.grid(row=2, column=15, sticky=W)
-        self.close = Button(self, text="Close Window", command=lambda: self.destroy())
-        self.close.grid(row=2, column=17, sticky=E, pady=(10, 10))
+        minus_hundred = Button(self, text="-100 ms", command=lambda: self.decrease_timing(100))
+        minus_hundred.grid(row=2, column=7, sticky=E)
+        minus_ten = Button(self, text="-10 ms", command=lambda: self.decrease_timing(10))
+        minus_ten.grid(row=2, column=8, sticky=E)
+        apply_timing = Button(self, text="Apply New Timing", command=self.apply_timing)
+        apply_timing.grid(row=2, column=9, columnspan=5, sticky=EW, padx=5)
+        plus_ten = Button(self, text="+10 ms", command=lambda: self.increase_timing(10))
+        plus_ten.grid(row=2, column=14, sticky=W)
+        plus_hundred = Button(self, text="+100 ms", command=lambda: self.increase_timing(100))
+        plus_hundred.grid(row=2, column=15, sticky=W)
+        close = Button(self, text="Close Window", command=lambda: self.destroy())
+        close.grid(row=2, column=17, sticky=E, pady=(10, 10))
         self.screen()
-
 
     def screen(self, *args):
         try:
@@ -42,15 +40,17 @@ class Preview(Toplevel):
         self.after(self.timing, lambda:self.screen(show))
 
     def increase_timing(self, ms):
+        if self.timing == 17:
+            ms -= 7
         self.timing += ms
 
     def decrease_timing(self, ms):
-        if self.timing >= 16:
+        if self.timing >= 17:
             self.timing -= ms
-        if self.timing < 16:
-            self.timing = 16
+        if self.timing < 17:
+            self.timing = 17
 
     def apply_timing(self):
-        self.output.delete(0, END)
-        timing_format = "{:.2f}".format(self.timing / 1000)
-        self.output.insert(0, timing_format)
+        # self.entry.delete(0, END)
+        timing_format = "{:.3f}".format(self.timing / 1000).rstrip('0')
+        self.entry.set(timing_format)
